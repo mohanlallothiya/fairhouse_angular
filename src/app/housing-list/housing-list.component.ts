c496ba12b198de35e1982eec1d48111a2e514b7d
Mohan Lal Lothiya <mohanjdh2016@gmail.com>
2023-02-19 19:07:34 +0530

    first commit

---
+++ src/app/housing-list/housing-list.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit,Input,Output, EventEmitter } from '@angular/core';
+import {HousingLocation } from '../housing-location'; 
+
+@Component({
+  selector: 'app-housing-list',
+  templateUrl: './housing-list.component.html',
+  styleUrls: ['./housing-list.component.css']
+})
+export class HousingListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  searchHousingLocations(searchText: string) {
+    this.results = this.locationList.filter(
+      (location: HousingLocation) => location.city
+        .toLowerCase()
+        .includes(
+            searchText.toLowerCase()
+      ));
+  };
+
+  selectHousingLocation(location: HousingLocation) {
+    this.locationSelectedEvent.emit(location);
+  };
+
+  @Input() locationList: HousingLocation[] = [];
+  results: HousingLocation[] = [];
+ 
+  @Output() locationSelectedEvent = new EventEmitter<HousingLocation>();
+}
